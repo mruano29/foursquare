befdcca975c4a4d79f83051da3cf24e963da95f5
Manu <mruanodevelopment@gmail.com>
2017-01-14 09:38:12 +0000

    webpack and package json updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var webpack = require('webpack');
+
 var config = {
   entry: path.resolve(__dirname, 'src/main.js'),
   output: {
@@ -6,58 +8,24 @@ var config = {
     filename: 'bundle.js',
     publicPath: "/build/"
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
+
   module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2016', 'react']
-        }
+    loaders: [{
+          test:    /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader:  'babel-loader',
+          query:   {
+              plugins: ['transform-runtime'],
+              presets: ['es2015', 'react'],
+          },
       },
       // SASS
       {
         test: /\.scss$/,
         loader: 'style!css!sass'
       },
-      {
-        test: /\.woff$/,
-        loader: 'url?limit=100000'
-      },
-      {
-        test: /\.eot$/,
-        loader: 'url?limit=100000'
-      },
-      {
-        test: /\.woff2$/,
-        loader: 'url?limit=100000'
-      },
-      {
-        test: /\.ttf$/,
-        loader: 'url?limit=100000'
-      },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
     ]
-  }
+  },
 };
 
 module.exports = config;
